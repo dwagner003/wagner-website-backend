d752c0a3d91543cc9ff1619864aa1e1fc50f391d
Devin Wagner <devinwagner003@gmail.com>
2020-05-11 21:09:57 -0600

    Adding start script in package.json for Heroku Hosting

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use((req, res, next) => {
 
 const db = require("./app/models");
 //This needs to go away before production like env.
-db.sequelize.sync({ force: true}).then(() => {
-    console.log("Dropping and syncing db.")
-});
+//db.sequelize.sync({ force: true}).then(() => {
+ //   console.log("Dropping and syncing db.")
+//});
 
 var corsOptions = {
     origin: "http://localhost:4200"
